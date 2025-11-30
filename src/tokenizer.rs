02c33da96bcd30563d40dc0692118097b952350d
Takeru Ohta <phjgt308@gmail.com>
2025-12-01 00:49:11 +0900

    refactor: remove henkan token type and converter

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -20,16 +20,7 @@ impl<'a> Tokenizer<'a> {
         Token::Sonomama { text }
     }
 
-    fn take_henkan_token<F>(&mut self, split: F) -> Token<'a>
-    where
-        F: FnOnce(&'a str) -> Option<(&'a str, &'a str)>,
-    {
-        let (text, remaining) = split(self.text).unwrap_or((self.text, ""));
-        self.text = remaining;
-        Token::Henkan { text }
-    }
-
-    fn take_hiragana_or_katakana_or_henkan_token(&mut self) -> Token<'a> {
+    fn take_hiragana_or_katakana_token(&mut self) -> Token<'a> {
         let pattern = |c: char| c == '_' || c.is_ascii_whitespace();
         let pos = self.text.find(pattern).unwrap_or(self.text.len());
         let text = &self.text[..pos];
@@ -77,13 +68,8 @@ impl<'a> Iterator for Tokenizer<'a> {
                     s.split_once(|c: char| c.is_ascii_whitespace())
                 }),
             )
-        } else if let Some(s) = self.text.strip_prefix(':')
-            && !self.text[1..].starts_with(|c: char| c.is_ascii_whitespace())
-        {
-            self.text = s;
-            Some(self.take_henkan_token(|s| s.split_once(|c: char| c.is_ascii_whitespace())))
         } else {
-            Some(self.take_hiragana_or_katakana_or_henkan_token())
+            Some(self.take_hiragana_or_katakana_token())
         }
     }
 }
@@ -97,9 +83,6 @@ pub enum Token<'a> {
     Katakana { text: &'a str },
     // Dictionary JSON: {"type": "kanji", "from": "にほんご", "to": ["日本語"]}
     Kanji { text: &'a str, index: usize },
-    // Dictionary JSON: {"type": "henkan", "from": "cat", "to": "🐱"}
-    // Dictionary JSON: {"type": "henkan", "from": "memory", "to": "メモリー"}
-    Henkan { text: &'a str },
 }
 
 impl<'a> nojson::DisplayJson for Token<'a> {
@@ -122,10 +105,6 @@ impl<'a> nojson::DisplayJson for Token<'a> {
                 f.member("text", text)?;
                 f.member("index", index)
             }
-            Token::Henkan { text } => {
-                f.member("type", "henkan")?;
-                f.member("text", text)
-            }
         })
     }
 }
@@ -147,8 +126,7 @@ mod tests {
             Token::Sonomama { text }
             | Token::Hiragana { text }
             | Token::Katakana { text }
-            | Token::Kanji { text, .. }
-            | Token::Henkan { text } => text,
+            | Token::Kanji { text, .. } => text,
         }
     }
 }
